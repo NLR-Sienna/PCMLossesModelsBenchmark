ebda804ad4af821a4fb65b583aa419a1e1c06ffa
rodrigomha <rodrigomha@gmail.com>
2026-02-13 10:37:01 -0800

    add formatter

---
+++ SiennaScripts/formatter/formatter_code.jl
@@ -0,0 +1,37 @@
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+Pkg.update()
+
+using JuliaFormatter
+
+main_paths = ["SiennaScripts"]
+for main_path in main_paths
+    for (root, dir, files) in walkdir(main_path)
+        for f in files
+            @show file_path = abspath(root, f)
+            !((occursin(".jl", f) || occursin(".md", f))) && continue
+            format(file_path;
+                whitespace_ops_in_indices = true,
+                remove_extra_newlines = true,
+                verbose = true,
+                always_for_in = true,
+                whitespace_typedefs = true,
+                conditional_to_if = true,
+                join_lines_based_on_source = true,
+                separate_kwargs_with_semicolon = true,
+                format_markdown = true,
+                # extending_powersimulations causes format failures but is not current public
+                ignore = [
+                    "README.md",
+                    "index.md",
+                    "extending_powersimulations.md",
+                    "simulation_recorder.md",
+                    "how_to/install.md",
+                ],
+
+                # always_use_return = true. # Disabled since it throws a lot of false positives
+            )
+        end
+    end
+end
